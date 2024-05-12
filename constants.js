0250a7cc77b339c688cd555dae855fa8d112f669
Syed Faateh Sultan Kazmi <faatehsultan@gmail.com>
2024-05-12 08:28:36 -0700

    Feat: Add setup socket io server

---
+++ constants.js
@@ -0,0 +1,5 @@
+const EVENT_TYPES = {
+  NEW_DELIVERY_REQUEST: "NEW_DELIVERY_REQUEST",
+};
+
+module.exports = { EVENT_TYPES };
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+
+const { EVENT_TYPES } = require("./constants");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+// internal routes for testing
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/test-client", (req, res) => {
+  res.sendFile(__dirname + "/public/test-client.html");
+});
+
+app.get("/socket.io/socket.io.js", (req, res) => {
+  res.sendFile(__dirname + "/node_modules/socket.io/client-dist/socket.io.js");
+});
+
+// socket io event handling
+io.on("connection", (socket) => {
+  console.log(`${socket?.id} new user connected`);
+
+  // api for backend to call on
+  app.post("/api/forward-emit", (req, res) => {
+    const data = req?.body;
+    console.log("data", data);
+    io.emit(EVENT_TYPES.NEW_DELIVERY_REQUEST, data);
+
+    res.sendStatus(200);
+  });
+
+  // FOR TESTING: MANUAL PING PONG
+  socket.on("ping", (data) => {
+    console.log("ping: " + data);
+    io.emit("pong", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`${socket?.id} user disconnected`);
+  });
+});
+
+// run server
+const PORT = process.env.PORT || 8001;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
